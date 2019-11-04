d559504f743f56bde4709fabc3ff01a3d5010bba
兰军 <lanjun@encdata.cn>
2019-11-04 09:42:05 +0800

    Hystrix修复熔断时的返回为NULL的问题

--- service-api/src/main/java/com/jedreck/serviceapi/service/StudentClientService.java
+++ service-api/src/main/java/com/jedreck/serviceapi/service/StudentClientService.java
@@ -1,6 +1,5 @@
 package com.jedreck.serviceapi.service;
 
-import com.jedreck.serviceapi.entities.Student;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,5 +12,5 @@ import java.util.List;
 @FeignClient(value = "${provider01.name}", fallbackFactory = StudentClientServiceFallbackFactory.class)
 public interface StudentClientService {
     @GetMapping(value = "provider/student/getAll")
-    List<Student> getAll();
+    List getAll();
 }
