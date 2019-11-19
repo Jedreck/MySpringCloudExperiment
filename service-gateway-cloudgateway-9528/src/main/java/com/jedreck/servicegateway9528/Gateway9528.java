53fd1aab00e023dfcb7a8737d00ed58945231ae2
兰军 <lanjun@encdata.cn>
2019-11-19 17:22:35 +0800

    gateway学习

---
+++ service-gateway-cloudgateway-9528/src/main/java/com/jedreck/servicegateway9528/Gateway9528.java
@@ -0,0 +1,15 @@
+package com.jedreck.servicegateway9528;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LanJun
+ * 2019/11/19 10:16
+ */
+@SpringBootApplication
+public class Gateway9528 {
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway9528.class, args);
+    }
+}
