7a2379cda912e63d0648d76cc8415210933cf74d
兰军 <lanjun@encdata.cn>
2020-01-15 10:05:12 +0800

    修改测试返回格式

--- service-provider-8003/src/main/java/com/jedreck/serviceprovider8003/controller/HelloWorldController.java
+++ service-provider-8003/src/main/java/com/jedreck/serviceprovider8003/controller/HelloWorldController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping()
     public String getAll() {
-        return "\"8001\":\"Hello World\"";
+        return "{\"8001\":\"Hello World\"}";
     }
 }
