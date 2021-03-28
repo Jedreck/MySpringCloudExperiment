ccdd908a23da5e3734c64641c2213b9c03f54fb4
兰军 <lanjun@encdata.cn>
2021-03-28 13:39:58 +0800

    图片格式判断png/jpg

---
+++ test-QRcode/src/main/java/com/jedreck/qrcode/zxingtest01/utils/ImageExtensionUtil.java
@@ -0,0 +1,48 @@
+package com.jedreck.qrcode.zxingtest01.utils;
+
+import com.jedreck.qrcode.zxingtest01.constants.MediaType;
+
+public class ImageExtensionUtil {
+    /**
+     * PNG识别符
+     */
+    private static final byte[] MARK_BUF_PNG = {(byte) 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+    /**
+     * JPEG开始符
+     */
+    private static final byte[] MARK_BUF_JPEG_HEAD = {(byte) 0xff, (byte) 0xd8};
+    /**
+     * JPEG结束符
+     */
+    private static final byte[] MARK_BUF_JPEG_FOOT = {(byte) 0xff, (byte) 0xd9};
+
+
+    public static MediaType getExtension(byte[] imageBytes) {
+        if (compare(imageBytes, MARK_BUF_PNG)) {
+            return MediaType.ImagePng;
+        } else if (compare(imageBytes, MARK_BUF_JPEG_HEAD) || compare(imageBytes, MARK_BUF_JPEG_FOOT)) {
+            return MediaType.ImageJpg;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 标示一致性比较
+     *
+     * @param buf     待检测标示
+     * @param markBuf 标识符字节数组
+     * @return 返回false标示标示不匹配
+     */
+    private static boolean compare(byte[] buf, byte[] markBuf) {
+        for (int i = 0; i < markBuf.length; i++) {
+            byte b = markBuf[i];
+            byte a = buf[i];
+
+            if (a != b) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
